c7b1bf8998ea72c806e44df400f471517688748e
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2025-08-24 17:52:00 +0530

    CSS Applied

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Thinula Area</title>
+    <title>Allien Area</title>
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    @vite('resources/css/app.css')
 </head>
 
-<body>
+<body class="text-center mt-4">
     <h1>Welcome to TJ Area</h1>
     <p>Click the button below to view the list of Areas </p>
     <a href="/areas" class='btn'>
