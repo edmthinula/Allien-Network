dfd1a034c106ff8f9a885e4289f18a1f684be893
Thinula Jayavihan <109309014+edmthinula@users.noreply.github.com>
2025-08-24 07:46:28 +0530

    routing

---
+++ resources/views/Areas/index.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Area Network | Home</title>
+</head>
+
+<body>
+    <h2>
+        Currently available Areas
+    </h2>
+    <ul>
+        <li>
+            Alliens here
+        </li>
+    </ul>
+</body>
+
+</html>
